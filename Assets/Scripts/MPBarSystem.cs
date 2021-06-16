5304dc1cf6aecbcd22e48fba56bbad25f938536d
cacapon <takuma.tsubo@amazingengine.co.jp>
2021-06-16 20:02:22 +0900

    HP、MPの表示形式をバーに変更

---
+++ Assets/Scripts/MPBarSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MPBarSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Slider SliderCurrentMP;
+
+
+    public float MaxMP = 100.0f;
+
+    public float MPChangeSpeed = 5.0f;
+
+    private ManaPoint MP;
+
+    private void Awake()
+    {
+        MP = new ManaPoint(MaxMP);
+        SliderCurrentMP.maxValue = MaxMP;
+    }
+
+    private void Update()
+    {
+        MP.ChangeMP(Time.deltaTime * MPChangeSpeed);
+        SliderCurrentMP.value = MP.CurrentMP;
+    }
+
+    public void SpendMP(int damagePoint)
+    {
+        MP.ChangeMP(-damagePoint);
+    }
+}
