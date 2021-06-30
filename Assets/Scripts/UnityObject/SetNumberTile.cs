333cb9961c675a1ba59843af35c1764476039f77
cacapon <takuma.tsubo@amazingengine.co.jp>
2021-06-30 09:29:47 +0900

    プレイヤーのHPを表示する機能を追加

---
+++ Assets/Scripts/UnityObject/SetNumberTile.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SetNumberTile : MonoBehaviour
+{
+    public PlayerData PlayerData;
+    public TileBase[] Numbers;
+
+    [SerializeField]
+    private Vector3Int Origin = new Vector3Int(-7,13,0); //FIXME: TileMapの原点位置の調整方法が分からないので、数字で調整しています。
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetCurrentHPTile();
+        SetMaxHPTile();
+    }
+
+    private void SetCurrentHPTile()
+    {
+        SetHPTile(1,PlayerData.InitHP); //TODO:CurrentHPをScriptableObject化していないため、暫定でINITHPを設定しています。
+    }
+
+    private void SetMaxHPTile()
+    {
+        SetHPTile(6, PlayerData.InitHP); //TODO:MaxHPをScriptableObject化していないため、暫定でINITHPを設定しています。
+    }
+
+
+    private void SetHPTile(int startXPosition, int hp)
+    {
+        /*
+            [イメージ]
+                O:icon X:CurrentHP Y:MaxHP
+                index   0123456789
+                        OXXXX/YYYY
+            HPを配列化したのちに、対象のインデックスに数字タイルを当てはめる処理を行います。
+        */
+        char[] chnums = hp.ToString().ToCharArray();
+
+        //初期化
+        for(int ite=0; ite<4; ite++)
+        {
+            this.GetComponent<Tilemap>().SetTile(Origin + new Vector3Int(startXPosition + ite, 0, 0), null);
+        }
+
+        if (1 <= chnums.Length && chnums.Length <= 4)
+        {
+            for (int index = 0; index < chnums.Length; index++)
+            {
+                int XPosition = startXPosition + 4 - chnums.Length + index;
+                this.GetComponent<Tilemap>().SetTile(Origin + new Vector3Int(XPosition, 0, 0), Numbers[(int)Char.GetNumericValue(chnums[index])]);
+            }
+        }
+    }
+}
