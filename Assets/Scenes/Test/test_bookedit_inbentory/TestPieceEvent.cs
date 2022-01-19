7a294ff5a909decf1686d1891282e2dff6813977
cacapon <takuma.tsubo@amazingengine.co.jp>
2022-01-19 18:28:31 +0900

    テスト用にカーソルをステージ上のカーソルを追いかけるように実装

---
+++ Assets/Scenes/Test/test_bookedit_inbentory/TestPieceEvent.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TestPieceEvent : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
+{
+    private eTile myTile;
+    private Sprite[] tiles;
+
+    private Image myImage;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myTile = eTile.YELLOW_PLUS;
+        myImage.sprite = tiles[((int)myTile)];
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myTile = eTile.None;
+        myImage.sprite = tiles[((int)myTile)];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTile = eTile.None;
+        myImage = GetComponent<Image>();
+        tiles = Resources.LoadAll<Sprite>("TestData/test_bookedit_inbentory/Tiles16x16");
+    }
+
+}
